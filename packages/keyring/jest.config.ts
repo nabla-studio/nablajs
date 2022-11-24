83c3bbf35836b9dbb2c4caf1bf25cf8c1f93c517
DavideSegullo <davidesegullo@gmail.com>
2022-11-24 01:03:47 +0100

    test(keyring): :wrench: add node as test environment

--- packages/keyring/jest.config.ts
+++ packages/keyring/jest.config.ts
@@ -1,15 +1,16 @@
 /* eslint-disable */
 export default {
-  displayName: 'keyring',
-  preset: '../../jest.preset.js',
-  globals: {
-    'ts-jest': {
-      tsconfig: '<rootDir>/tsconfig.spec.json',
-    },
-  },
-  transform: {
-    '^.+\\.[tj]s$': 'ts-jest',
-  },
-  moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/packages/keyring',
+	displayName: 'keyring',
+	testEnvironment: 'node',
+	preset: '../../jest.preset.js',
+	globals: {
+		'ts-jest': {
+			tsconfig: '<rootDir>/tsconfig.spec.json',
+		},
+	},
+	transform: {
+		'^.+\\.[tj]s$': 'ts-jest',
+	},
+	moduleFileExtensions: ['ts', 'js', 'html'],
+	coverageDirectory: '../../coverage/packages/keyring',
 };
