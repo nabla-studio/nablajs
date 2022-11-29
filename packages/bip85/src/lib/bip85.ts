c7f5320f9a70aa005d2d2c5cd032bef2f944cdb2
DavideSegullo <davidesegullo@gmail.com>
2022-11-29 23:16:37 +0100

    feat(bip85): :heavy_plus_sign: add basic deps for bip85

--- packages/bip85/src/lib/bip85.ts
+++ packages/bip85/src/lib/bip85.ts
@@ -1,3 +1,10 @@
-export function bip85(): string {
-	return 'bip85';
-}
+import { base58 } from '@scure/base';
+import { HDKey } from '@scure/bip32';
+import { mnemonicToSeed, entropyToMnemonic } from '@scure/bip39';
+import { sha256 } from '@noble/hashes/sha256';
+import { hmac } from '@noble/hashes/hmac';
+
+// wif https://github.com/bitcoinjs/wif/blob/master/index.js
+// TODO: create custom wif
+
+export class BIP85 {}
