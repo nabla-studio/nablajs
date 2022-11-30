bd1069b14c064561806913dc4068401f58f58c86
DavideSegullo <davidesegullo@gmail.com>
2022-11-30 15:28:43 +0100

    test(wif): :white_check_mark: add tests porting with jest

--- packages/wif/src/lib/wif.spec.ts
+++
@@ -1,7 +0,0 @@
-import { wif } from './wif';
-
-describe('wif', () => {
-	it('should work', () => {
-		expect(wif()).toEqual('wif');
-	});
-});
---
+++ packages/wif/src/test/wif.spec.ts
@@ -0,0 +1,57 @@
+import { encode, decode } from '../lib/wif';
+import { valid, invalid } from './data';
+
+describe('WIF Testing', () => {
+	for (const f of valid) {
+		it(`encode/encodeRaw returns ${f.WIF} for ${f.privateKeyHex.slice(
+			0,
+			20,
+		)}... (${f.version})`, () => {
+			const privateKey = Buffer.from(f.privateKeyHex, 'hex');
+			const actual = encode(f.version, privateKey, f.compressed);
+
+			expect(actual).toEqual(f.WIF);
+		});
+	}
+
+	for (const f of valid) {
+		it(`decode/decodeRaw returns ${f.privateKeyHex.slice(0, 20)}... (${
+			f.version
+		}) for ${f.WIF}`, () => {
+			const actual = decode(f.WIF, f.version);
+
+			expect(actual.version).toEqual(f.version);
+			expect(Buffer.from(actual.privateKey).toString('hex')).toEqual(
+				f.privateKeyHex,
+			);
+			expect(actual.compressed).toEqual(f.compressed);
+		});
+	}
+
+	for (const f of invalid.encode) {
+		it(`throws ${f.exception} for ${f.privateKeyHex}`, () => {
+			expect(() => encode(f.version, Buffer.from(f.privateKeyHex, 'hex'))).toThrow(
+				new RegExp(f.exception),
+			);
+		});
+	}
+
+	for (const f of invalid.decode) {
+		it(`throws ${f.exception} for ${f.WIF}`, () => {
+			expect(() => decode(f.WIF, f.version)).toThrow(new RegExp(f.exception));
+		});
+	}
+
+	for (const f of valid) {
+		it(`decode/encode for ${f.WIF}`, () => {
+			const decoded = decode(f.WIF, f.version);
+			const actual = encode(
+				decoded.version,
+				Buffer.from(decoded.privateKey),
+				decoded.compressed,
+			);
+
+			expect(actual).toEqual(f.WIF);
+		});
+	}
+});
