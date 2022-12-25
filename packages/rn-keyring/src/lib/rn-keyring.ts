3b76d7daf9a49f136ef902c16601aee4fd73bf18
DavideSegullo <davidesegullo@gmail.com>
2022-12-25 19:42:40 +0100

    feat(rn-keyring): :zap: improve performance for DirectSecp256k1HdWallet
    improved performance for wallet generation, because pbkdf2 from @noble/hash is very slow on RN.
    
    #9

--- packages/rn-keyring/src/lib/rn-keyring.ts
+++ packages/rn-keyring/src/lib/rn-keyring.ts
@@ -1,25 +1,67 @@
+import { EnglishMnemonic, HdPath } from '@cosmjs/crypto';
+import { DirectSecp256k1HdWallet } from '@cosmjs/proto-signing';
 import {
 	EncryptResponse,
 	Keyring,
 	KeyringStorage,
 	Nullable,
+	Wallet,
 	WalletOptions,
 } from '@nabla-studio/keyring';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import AES from 'react-native-aes-crypto';
-import { AESEcrypted, AESMetadata } from './types';
+import {
+	AESEcrypted,
+	AESMetadata,
+	AESStorageOptions,
+	AESWalletOptions,
+} from './types';
 
 export class RNKeyring extends Keyring<AESMetadata> {
 	constructor(
 		public override storageKey: string,
 		public override walletsOptions: WalletOptions[],
 		public salt: string,
-		public pbkdf2cost: number = 5000,
-		public pbkdf2length: number = 256,
-		public randomKeyLength: number = 16,
-		public algorithm: AES.Algorithms = 'aes-256-cbc',
+		public storageOptions: AESStorageOptions = {
+			pbkdf2cost: 5000,
+			pbkdf2length: 256,
+			randomKeyLength: 16,
+			algorithm: 'aes-256-cbc',
+		},
+		public walletOptions: AESWalletOptions = {
+			pbkdf2cost: 2048,
+			pbkdf2length: 64,
+		},
 	) {
 		super(storageKey, walletsOptions);
+	}
+
+	public override async generateWalletFromMnemonic(
+		mnemonic: string,
+		hdPaths: HdPath[],
+		prefix: string,
+	): Promise<Wallet> {
+		const mnemonicChecked = new EnglishMnemonic(mnemonic);
+		const seed = await this.mnemonicToSeed(mnemonic);
+
+		/**
+		 * We must ignore this, because the construct of  `DirectSecp256k1HdWallet` is protected, but this is the only way
+		 * to create a `DirectSecp256k1HdWallet` instance without using `pbkdf2` from `@noble/hash`, which is very slow on react native
+		 */
+		// @ts-ignore
+		const wallet: DirectSecp256k1HdWallet = new DirectSecp256k1HdWallet(
+			mnemonicChecked,
+			{
+				seed,
+				hdPaths,
+				prefix,
+			},
+		);
+
+		return {
+			wallet,
+			prefix,
+		};
 	}
 
 	protected async read(
@@ -86,17 +128,22 @@ export class RNKeyring extends Keyring<AESMetadata> {
 		const key = await AES.pbkdf2(
 			password,
 			this.salt,
-			this.pbkdf2cost,
-			this.pbkdf2length,
+			this.storageOptions.pbkdf2cost,
+			this.storageOptions.pbkdf2length,
 		);
 
 		return key;
 	}
 
 	private async encryptData(text: string, key: string) {
-		const iv = await AES.randomKey(this.randomKeyLength);
+		const iv = await AES.randomKey(this.storageOptions.randomKeyLength);
 
-		const cipher = await AES.encrypt(text, key, iv, this.algorithm);
+		const cipher = await AES.encrypt(
+			text,
+			key,
+			iv,
+			this.storageOptions.algorithm,
+		);
 
 		return {
 			cipher,
@@ -109,9 +156,36 @@ export class RNKeyring extends Keyring<AESMetadata> {
 			encryptedData.cipherText,
 			key,
 			encryptedData.iv,
-			this.algorithm,
+			this.storageOptions.algorithm,
 		);
 
 		return decrypt;
+	}
+
+	private nfkd(str: string) {
+		if (typeof str !== 'string') {
+			throw new TypeError(`Invalid mnemonic type: ${typeof str}`);
+		}
+		return str.normalize('NFKD');
+	}
+
+	private normalize(str: string) {
+		const norm = this.nfkd(str);
+		const words = norm.split(' ');
+		if (![12, 15, 18, 21, 24].includes(words.length)) {
+			throw new Error('Invalid mnemonic');
+		}
+		return { nfkd: norm, words };
+	}
+
+	private async mnemonicToSeed(mnemonic: string, passphrase = '') {
+		const salt = 'mnemonic' + (passphrase ? this.normalize(passphrase).nfkd : '');
+
+		return await AES.pbkdf2(
+			this.normalize(mnemonic).nfkd,
+			salt,
+			this.walletOptions.pbkdf2cost,
+			this.walletOptions.pbkdf2length,
+		);
 	}
 }
--- packages/rn-keyring/src/lib/types/aes.ts
+++ packages/rn-keyring/src/lib/types/aes.ts
@@ -1,7 +1,21 @@
+import AES from 'react-native-aes-crypto';
+
 export interface AESMetadata {
 	iv: string;
 }
 
 export interface AESEcrypted extends AESMetadata {
 	cipherText: string;
+}
+
+export interface AESWalletOptions {
+	pbkdf2cost: number;
+	pbkdf2length: number;
+}
+
+export interface AESStorageOptions {
+	pbkdf2cost: number;
+	pbkdf2length: number;
+	randomKeyLength: number;
+	algorithm: AES.Algorithms;
 }
