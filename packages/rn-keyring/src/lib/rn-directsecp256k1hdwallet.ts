08fafef16509491a808c19612eac6baee867935e
DavideSegullo <davidesegullo@gmail.com>
2022-12-27 22:01:15 +0100

    refactor(rn-keyring): :recycle: refactor using utils package
    refactored `RNDirectSecp256k1HdWallet` using mnemonicToSeed from utils package.
    
    #9

--- packages/rn-keyring/src/lib/rn-directsecp256k1hdwallet.ts
+++ packages/rn-keyring/src/lib/rn-directsecp256k1hdwallet.ts
@@ -3,8 +3,8 @@ import {
 	DirectSecp256k1HdWalletOptions,
 } from '@cosmjs/proto-signing';
 import { EnglishMnemonic } from '@cosmjs/crypto';
-import AES from 'react-native-aes-crypto';
 import { stringToBytes } from '@scure/base';
+import { mnemonicToSeed } from '@nabla-studio/utils';
 
 /**
  * We must implement this, because the construct of  `DirectSecp256k1HdWallet` is protected, and this is the only way
@@ -18,7 +18,7 @@ export class RNDirectSecp256k1HdWallet extends DirectSecp256k1HdWallet {
 		pbkdf2length = 64,
 	): Promise<DirectSecp256k1HdWallet> {
 		const mnemonicChecked = new EnglishMnemonic(mnemonic);
-		const seed = await this.mnemonicToSeed(
+		const seed = await mnemonicToSeed(
 			mnemonic,
 			options?.bip39Password ?? '',
 			pbkdf2cost,
@@ -29,37 +29,5 @@ export class RNDirectSecp256k1HdWallet extends DirectSecp256k1HdWallet {
 			...options,
 			seed: stringToBytes('utf8', seed),
 		});
-	}
-
-	private static nfkd(str: string) {
-		if (typeof str !== 'string') {
-			throw new TypeError(`Invalid mnemonic type: ${typeof str}`);
-		}
-		return str.normalize('NFKD');
-	}
-
-	private static normalize(str: string) {
-		const norm = this.nfkd(str);
-		const words = norm.split(' ');
-		if (![12, 15, 18, 21, 24].includes(words.length)) {
-			throw new Error('Invalid mnemonic');
-		}
-		return { nfkd: norm, words };
-	}
-
-	private static async mnemonicToSeed(
-		mnemonic: string,
-		passphrase = '',
-		pbkdf2cost = 2048,
-		pbkdf2length = 64,
-	) {
-		const salt = 'mnemonic' + (passphrase ? this.normalize(passphrase).nfkd : '');
-
-		return await AES.pbkdf2(
-			this.normalize(mnemonic).nfkd,
-			salt,
-			pbkdf2cost,
-			pbkdf2length,
-		);
 	}
 }
