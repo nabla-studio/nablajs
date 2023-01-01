e9e6cd35454cbdc68e206af396d3ac5eb7258d95
DavideSegullo <davidesegullo@gmail.com>
2023-01-01 14:20:30 +0100

    docs(rn-keyring): :memo: add doc for pbkdf2 spec
    close #20

--- packages/rn-keyring/src/lib/types/aes.ts
+++ packages/rn-keyring/src/lib/types/aes.ts
@@ -10,11 +10,17 @@ export interface AESEcrypted extends AESMetadata {
 
 export interface AESWalletOptions {
 	pbkdf2cost: number;
+	/**
+	 * PBKDF2 length defined in bits
+	 */
 	pbkdf2length: number;
 }
 
 export interface AESStorageOptions {
 	pbkdf2cost: number;
+	/**
+	 * PBKDF2 length defined in bits
+	 */
 	pbkdf2length: number;
 	randomKeyLength: number;
 	algorithm: AES.Algorithms;
