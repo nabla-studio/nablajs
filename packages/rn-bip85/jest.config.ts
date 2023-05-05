a3212ddbe7d1a785fb8effe68ce86d7144615dc9
DavideSegullo <davidesegullo@gmail.com>
2023-05-05 19:11:20 +0200

    feat: :arrow_up: migrate to nx 16

--- packages/rn-bip85/jest.config.ts
+++ packages/rn-bip85/jest.config.ts
@@ -1,11 +1,11 @@
 module.exports = {
 	displayName: 'rn-bip85',
 	preset: 'react-native',
-	resolver: '@nrwl/jest/plugins/resolver',
+	resolver: '@nx/jest/plugins/resolver',
 	moduleFileExtensions: ['ts', 'js', 'html', 'tsx', 'jsx'],
 	setupFilesAfterEnv: ['<rootDir>/test-setup.ts'],
 	moduleNameMapper: {
-		'.svg': '@nrwl/react-native/plugins/jest/svg-mock',
+		'.svg': '@nx/react-native/plugins/jest/svg-mock',
 	},
 	coverageDirectory: '../../coverage/packages/rn-bip85',
 };
--- packages/rn-keyring/jest.config.ts
+++ packages/rn-keyring/jest.config.ts
@@ -1,11 +1,11 @@
 module.exports = {
 	displayName: 'rn-keyring',
 	preset: 'react-native',
-	resolver: '@nrwl/jest/plugins/resolver',
+	resolver: '@nx/jest/plugins/resolver',
 	moduleFileExtensions: ['ts', 'js', 'html', 'tsx', 'jsx'],
 	setupFilesAfterEnv: ['<rootDir>/test-setup.ts'],
 	moduleNameMapper: {
-		'.svg': '@nrwl/react-native/plugins/jest/svg-mock',
+		'.svg': '@nx/react-native/plugins/jest/svg-mock',
 	},
 	coverageDirectory: '../../coverage/packages/rn-keyring',
 };
